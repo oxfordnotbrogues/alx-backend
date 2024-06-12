cb4e71ce20f6429351ce0366db59857f6604d7f5
oxfordnotbrogues <ifeomaonyeabo33@gmail.com>
2024-06-12 15:35:48 -0700

    queuing system

---
+++ 0x03-queuing_system_in_js/9-stock.js
@@ -0,0 +1,39 @@
+#!/usr/bin/yarn dev
+import express from 'express';
+import { promisify } from 'util';
+import { createClient } from 'redis';
+
+const listProducts = [
+  {
+    itemId: 1,
+    itemName: 'Suitcase 250',
+    price: 50,
+    initialAvailableQuantity: 4
+  },
+  {
+    itemId: 2,
+    itemName: 'Suitcase 450',
+    price: 100,
+    initialAvailableQuantity: 10
+  },
+  {
+    itemId: 3,
+    itemName: 'Suitcase 650',
+    price: 350,
+    initialAvailableQuantity: 2
+  },
+  {
+    itemId: 4,
+    itemName: 'Suitcase 1050',
+    price: 550,
+    initialAvailableQuantity: 5
+  },
+];
+
+const getItemById = (id) => {
+  const item = listProducts.find(obj => obj.itemId === id);
+
+  if (item) {
+    return Object.fromEntries(Object.entries(item));
+  }
+};
